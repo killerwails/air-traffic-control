266480185b05b8cf9d99e8dea8c1617b5205ea55
steven harradine <steven.harradine@telus.com>
2015-07-18 23:30:24 -0400

    fix user id, add slack token

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-module.exports.PORT = 3000
+module.exports.PORT            = 3000
 module.exports.REPOSITORY_HOME = '/home/douglas/Repository'
-module.exports.USERID = 't864007'
+module.exports.USERID          = 'ubuntu'
+module.exports.SLACK_TOKEN     = "TOKEN/TOKEN/TOKEN"
