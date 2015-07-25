2c44a5a9ddc66d51ea0083803340da6073e91210
Steven Harradine <stevenharradine@gmail.com>
2015-07-25 02:19:47 -0400

    handle favico in env, handle errors in getFiles, read hosts for roles per project for hotswapping boxes (because you cant hotswap the entire env and validate at the same time)

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ if (cluster.isMaster) {
         playbook     = url_folders[3],
         role         = url_folders[4]
 
+    if (env == "favicon.ico") env = ""
+
     if (env == "") {
       buffered_out += showEnviromentSelection ()
     } else if (action == "build") {
@@ -58,16 +60,21 @@ function replaceAll(find, replace, str) {
 }
 
 function getFiles (dir, files_){
-    files_    = files_ || []
-    var files = fs.readdirSync(dir)
+    files_ = files_ || []
 
-    for (var i in files) {
-        var name = dir + '/' + files[i]
+    try {
+      var files = fs.readdirSync(dir)
 
-        files_.push(name)
+      for (var i in files) {
+          var name = dir + '/' + files[i]
+
+          files_.push(name)
+      }
+
+      return files_
+    } catch (e) {
+      return -1
     }
-
-    return files_
 }
 
 function sendSlack (message) {
@@ -176,40 +183,49 @@ function hotswap (playbook, env, role) {
 }
 
 function showEnviromentSelection () {
-  var buffered_out = "<h1>Select enviroment</h1>" +
-                     "  <ul>" +
-                     "    <li><a href='next/'>next</a></li>" +
-                     "    <li><a href='development/'>development</a></li>" +
-                     "    <li><a href='staging/'>staging</a></li>" +
-                     "    <li><a href='production/'>production</a></li>" +
-                     "  </ul>"
+  var buffered_out = "<h1>Select enviroment</h1>"
+                   + "  <ul>"
+                   + "    <li><a href='next/'>next</a></li>"
+                   + "    <li><a href='development/'>development</a></li>"
+                   + "    <li><a href='staging/'>staging</a></li>"
+                   + "    <li><a href='production/'>production</a></li>"
+                   + "  </ul>"
 
   return buffered_out
 }
 
 function showIndex (enviroment) {
-  var files        = getFiles(CONFIG.REPOSITORY_HOME),
-      buffered_out = "<h1><a href='../'>&lt;</a>" + enviroment + "</h1>" +
-                     "<table>" +
-                     "  <tr>" +
-                     "    <th>Playbook</th>" +
-                     "    <th></th>" +
-                     "  </tr>"
+  var playbook_files = getFiles(CONFIG.REPOSITORY_HOME),
+      buffered_out   = "<h1><a href='../'>&lt;</a>" + enviroment + "</h1>"
+                     + "<table>"
+                     + "  <tr>"
+                     + "    <th>Playbook</th>"
+                     + "    <th></th>"
+                     + "  </tr>"
 
-  for (var i in files) {
-    var file = files[i].replace (CONFIG.REPOSITORY_HOME + "/", "")
+  for (var i in playbook_files) {
+    var playbook_file = playbook_files[i].replace (CONFIG.REPOSITORY_HOME + "/", "")
 
-    if (file.indexOf ("playbook-") == 0) {
-      var this_playbook = file.replace ("playbook-", "")
+    if (playbook_file.indexOf ("playbook-") == 0) {
+      var this_playbook = playbook_file.replace ("playbook-", ""),
+          roles         = sh.exec ("cat " + CONFIG.REPOSITORY_HOME + "/playbook-" + this_playbook + "/hosts/" + enviroment + " | grep teluswebteam.com").stdout.split(/\r\n|\r|\n/g)
 
-      buffered_out += "<tr>" +
-                      "  <td>" + this_playbook + "</td>" +
-                      "  <td>" + 
-                      "    <a href='/" + enviroment + "/build/" + this_playbook + "'>build infra</a> | " +
-                      "    <a href='/" + enviroment + "/reforge/" + this_playbook + "'>reforge</a> | " + 
-                      "    hotswap: <a href='/" + enviroment + "/hotswap/" + this_playbook + "/inbound'>Inbound</a>" +
-                      "  </td>"
-                      "</tr>"
+      buffered_out += "<tr>"
+                    + "  <td>" + this_playbook + "</td>"
+                    + "  <td>"
+                    + "    <a href='/" + enviroment + "/build/" + this_playbook + "'>build infra</a> | "
+                    + "    <a href='/" + enviroment + "/reforge/" + this_playbook + "'>reforge</a> | "
+                    + "    hotswap: "
+
+      for (var r in roles) {
+        var role = roles[r].split(".")[0]
+
+        if (role.indexOf("cat:") != 0)
+          buffered_out += "      <a href='/" + enviroment + "/hotswap/" + this_playbook + "/" + role + "'>" + role + "</a>"
+      }
+
+      buffered_out += "  </td>"
+                    + "</tr>"
     }
   }
 
