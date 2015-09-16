561ec6f075c749fb485a7b9c215f4e397844e858
steven harradine <steven.harradine@telus.com>
2015-09-16 10:02:36 -0400

    update REPO HOME

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports.PORT            = 3000
-module.exports.REPOSITORY_HOME = '/home/douglas/Repositories'
+module.exports.REPOSITORY_HOME = '/home/douglas/Repository'
 module.exports.USERID          = 'ubuntu'
 module.exports.SLACK_TOKEN     = "TOKEN/TOKEN/TOKEN"
 module.exports.CPU_MULTIPLIER  = 1
